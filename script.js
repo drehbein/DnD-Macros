4b3d02f9084195285c111d7d2680557fd0982c90
drehbein <devionrehbein@gmail.com>
2023-10-22 01:28:36 -0500

    Added option selector

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
 		{
 			name: "Prophet",
 			description: "Badass talking halberd",
+			uniqueOptions: [
+				{name: "Knockback", checked: false, effect: ["Psychic", "1d8"]}
+			],
 			toHit: "10",
 			damage: "1d10+6"
 		}, {
 			name: "Unarmed strike",
 			description: "Lame",
+			uniqueOptions: [],
 			toHit: "9",
 			damage: "6"
 		}
-	]
+	];
+	
+	console.log(weapons);
 
 	// Display weapon selection
 	for (let i = 0; i < weapons.length; i++) {
@@ -33,7 +39,39 @@ function chooseWeapon(choice) {
 	description = document.getElementById("description");
 	description.removeAttribute("hidden");
 
-	// Displays applicable options
+	// Collects applicable options
+	standardOptions = [
+		{name: "Regular Action", checked: true, effect: ["Attacks", 3]},
+		{name: "Action Surge", checked: false, effect: ["Attacks", 3]},
+		{name: "Bonus Attack", checked: false, effect: ["Bonus Attacks", 1]},
+		{name: "Opportunity Attack", checked: false, effect: ["Attacks", 1]}
+	];
+	options = standardOptions.concat(weapon.uniqueOptions);
+	
+	// Constructs options interface
+	var optionsRow = document.createElement("TR");
+	optionsRow.setAttribute("id", "optionsRow");
+	var optionsTable = document.getElementById("optionsTable");
+	optionsTable.innerHTML = "";
+	optionsTable.appendChild(optionsRow);
+
+	for (let i=0; i < options.length; i++) {
+		let dataCell = document.createElement("TD");
+		let input = document.createElement("INPUT");
+		input.setAttribute("type", "checkbox");
+		input.setAttribute("id", "option" + (i+1));
+		input.setAttribute("name", "attackOptions");
+		input.setAttribute("value", options[i].name);
+		dataCell.appendChild(input);
+		input.checked = options[i].checked;
+		let label = document.createElement("LABEL");
+		label.setAttribute("for", "option" + (i+1));
+		label.innerHTML = " " + options[i].name;
+		dataCell.appendChild(label);
+		optionsRow.appendChild(dataCell);
+	}
+	console.log(optionsRow);
+	optionsCont.removeAttribute("hidden");
 }
 
 // Test
