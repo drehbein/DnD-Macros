1117e3bff7d1331fc078565c6d315a4da57a76bd
drehbein <devionrehbein@gmail.com>
2023-11-04 04:21:54 -0500

    Made user input parsing that calculates individual rolls

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 // Function to handle rolling, referencing other functions for each step
-// Function to parse user input turning "ToHit: +10 || Physical: 1d10+6 || Psychic: 1d8" into "ToHit: # || Physical: # || Psychic: #"
+function roll() {
+	inputReceiver = document.getElementById("input")
+	input = inputReceiver.value;
+	output = parseInput(input);
+	print(output);
+}
+
+// Function to transform user input like "ToHit: +10 || Physical: 1d10+6 || Psychic: 1d8" into "ToHit: # || Physical: # || Psychic: #"
+function parseInput(input) {
+	effectsArray = input.split(/ \|\| |\: /);
+	output = "";
+	while (effectsArray.length) {
+		effectLabel = effectsArray.shift();
+		output += effectLabel + ": ";
+		effectRoll = effectsArray.shift();
+		effectValue = evaluateRandoms(effectRoll.split("+"));
+		output += effectValue;
+		if (effectsArray.length) {
+			output += " || ";
+		}
+	}
+	return(output);
+}
+
+// Calculates individual rolls (1d20+6 => 26)
+function evaluateRandoms(roll) {
+	rollValue = 0;
+	roll.forEach(function(currentValue, index, array) {
+		if (currentValue.includes("d")) {
+			dieQuantity = parseInt(currentValue.match(/^(\d+)d\d+$/)[1], 10);
+			dieSides = parseInt(currentValue.match(/^\d+d(\d+)$/)[1], 10);
+			for (let i = 0; i < dieQuantity; i++) {
+				rollValue += Math.floor(Math.random() * dieSides) + 1;
+			}
+		} else {
+			rollValue += parseInt(currentValue);
+		}
+	});
+	return(rollValue);
+}
+
 // Function to print result to output/log (start as simply setting inner html, then change to adding to a cumulative log, then make log save to local storage)
+function print(output) {
+	outputDisplay = document.getElementById("output");
+	let row = document.createElement("tr");
+	row.innerHTML = output;
+	outputDisplay.appendChild(row);
+}
